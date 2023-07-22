70416dbeb7f372538202259e12005820cc71c035
atrislab <atrislab@gmail.com>
2023-07-22 18:17:40 +0200

    Sass installed and running

--- merny_front/src/App.jsx
+++ merny_front/src/App.jsx
@@ -1,4 +1,5 @@
-import './App.css'
+//import './App.css'
+import './Sass/App.scss'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
